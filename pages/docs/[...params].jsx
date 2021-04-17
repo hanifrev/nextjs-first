a48bd4f8cdad701d27b6123356d3f42736a9260c
hanifrev <hanifrev@gmail.com>
2021-04-17 13:19:50 +0700

    fix

--- pages/docs/[...params].jsx
+++ pages/docs/[...params].jsx
@@ -1,10 +1,20 @@
-// import React from "react";
+import React from "react";
 
-export async function getStaticProps(context) {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
-  const data = await res.json();
+// export async function getStaticProps(context) {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
+//   const data = await res.json();
 
-  return {
-    props: {},
-  };
-}
+//   return {
+//     props: {},
+//   };
+// }
+
+const mayh = () => {
+  return (
+    <div>
+      <p>my arms your hearse</p>
+    </div>
+  );
+};
+
+export default mayh;

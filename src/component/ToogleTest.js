8e9bd7ac3c55118d75badd1490b64129b0bd7d6a
hanifrev <hanifrev@gmail.com>
2021-12-18 08:02:08 +0700

    simple toogle style

--- src/component/ToogleTest.js
+++ src/component/ToogleTest.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-// import Image from 'next/image'
 
 const ToogleTest = () => {
     const [show, setShow] = useState(false)
@@ -7,14 +6,36 @@ const ToogleTest = () => {
         return (
             <div>
                 <img src="https://www.kabarpenumpang.com/wp-content/uploads/2017/06/lK61rWG.jpg" alt="test" />
+                <ul>
+                    <li>test 1</li>
+                    <li>test 2</li>
+                    <li>test 3</li>
+                    <li>test 4</li>
+                    <li>test 5</li>
+                </ul>
             </div>
         )
     }
 
+    const styles = {
+        "backgroundColor": "red",
+        "color": "white"
+    }
+
+    const styles2 = {
+        "backgroundColor": "green",
+        "color": "white"
+    }
+
     return (
         <div>
+            <button
+                style={show ? styles : styles2}  
+                onClick={() => setShow(!show)}
+            >
+                {show ? "Close" : "Open"}
+            </button>
             {show && <Imgs />}
-            <button onClick={() => setShow(!show)}>OPEN</button>
         </div>
     )
 }

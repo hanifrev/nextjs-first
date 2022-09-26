0b4f737725a9a2ff01d1ac762e6e0c21ac6f8d7c
hanifrev <hanifrev@gmail.com>
2022-09-26 21:20:17 +0700

    search country

--- pages/notes/Country/index.js
+++ pages/notes/Country/index.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import Link from "next/link";
 
 const index = ({ country }) => {
+  const [search, setSearch] = useState("");
+
+  const list = {
+    display: "grid",
+    gridTemplateColumns: "auto auto auto auto",
+    gap: "4rem",
+    paddingTop: "1rem",
+  };
+
   return (
     <div>
-      {country.map((x, index) => {
-        return <div>{x.name.common}</div>;
-      })}
+      <div>
+        <input
+          type="text"
+          placeholder="Search Here"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+        />
+      </div>
+      <div style={list}>
+        {country
+          .filter((val) => {
+            if (search == "") {
+              return val;
+            } else if (
+              val.name.common.toLowerCase().includes(search.toLowerCase())
+            ) {
+              return val;
+            }
+          })
+          .map((x, index) => {
+            return (
+              <Link
+                href={`https://restcountries.com/v3.1/name/${x.name.common}`}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    width: "11rem",
+                    cursor: "pointer",
+                  }}
+                >
+                  <img style={{ width: "150px" }} src={x.flags.svg}></img>
+                  {x.name.common}
+                </div>
+              </Link>
+            );
+          })}
+      </div>
     </div>
   );
 };

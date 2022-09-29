c55231da8c329d2f3771d7185d3f6b0a7dfc246e
hanifrev <hanifrev@gmail.com>
2022-09-29 09:42:46 +0700

    country fromTo test

--- pages/notes/Country/index.js
+++ pages/notes/Country/index.js
@@ -1,21 +1,50 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState, useLayoutEffect } from "react";
 import Link from "next/link";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 const index = ({ country }) => {
   const [search, setSearch] = useState("");
 
   const theRef = useRef();
+  gsap.registerPlugin(ScrollTrigger);
 
   const x = gsap.utils.selector(theRef);
 
-  useEffect(() => {
-    gsap.from(x(".theList"), {
-      y: 200,
-      duration: 1.5,
-      stagger: 0.1,
-      opacity: 0,
-    });
+  // useEffect(() => {
+  //   gsap.from(x(".theList"), {
+  //     y: 200,
+  //     duration: 1.5,
+  //     stagger: 0.1,
+  //     opacity: 0,
+  //   });
+  // }, []);
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context((self) => {
+      const boxes = self.selector(".theList");
+      boxes.forEach((box) => {
+        gsap.fromTo(
+          box,
+          {
+            y: 200,
+            opacity: 0,
+          },
+          {
+            y: 0,
+            duration: 2,
+            opacity: 1,
+            scrollTrigger: {
+              id: 1,
+              trigger: box,
+              start: "top bottom",
+              toggleActions: "play none none reverse",
+            },
+          }
+        );
+      });
+    }, theRef);
+    return () => ctx.revert();
   }, []);
 
   const list = {
